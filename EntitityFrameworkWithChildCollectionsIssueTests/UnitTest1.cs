31defe40a4d25d44496dbd736b0911e890e01d9f
Schorpp, Max (DF FA AS DH KHE 2) <max.schorpp@siemens.com>
2022-10-20 15:53:52 +0200

    dedicated childre can be reused and refenced multiple times without a problem

--- EntitityFrameworkWithChildCollectionsIssueTests/UnitTest1.cs
+++ EntitityFrameworkWithChildCollectionsIssueTests/UnitTest1.cs
@@ -5,9 +5,14 @@ namespace EntitityFrameworkWithChildCollectionsIssueTests
 {
     public class Tests
     {
-        record TestData(string Name, IList<TestSubData> Children, long Id = default)
+        record TestDataWithListOfChildren(string Name, IList<TestSubData> Children, long Id = default)
         {
-            public TestData() : this(default, default) { }
+            public TestDataWithListOfChildren() : this(default, default) { }
+        };
+
+        record TestDataWithTwoChildren(string Name, TestSubData Child1, TestSubData Child2, long Id = default)
+        {
+            public TestDataWithTwoChildren() : this(default, default, default) { }
         };
 
         record TestSubData(string Name, long Id = default)
@@ -18,35 +23,78 @@ namespace EntitityFrameworkWithChildCollectionsIssueTests
 
         class TestDb : DbContext
         {
-            public DbSet<TestData> TestData { get; set; } = default!;
+            public DbSet<TestDataWithListOfChildren> TestDataWithListOfChildren { get; set; } = default!;
             public DbSet<TestSubData> TestSubData { get; set; } = default!;
+            public DbSet<TestDataWithTwoChildren> TestDataWithTwoChildren { get; set; } = default!;
 
             public TestDb() : base(new DbContextOptionsBuilder().UseInMemoryDatabase("testDb", new InMemoryDatabaseRoot()).Options) 
             { }
 
             protected override void OnModelCreating(ModelBuilder modelBuilder)
-            { 
+            {
             }
         }
 
         [Test]
-        public async Task Test1()
+        public async Task Test_WithListOfChildren()
         {
             // Create DB
             using TestDb testDb = new();
 
             // define data
             var sharedChild = new TestSubData("Shared", 1);
-            var entry1 = new TestData("Max", 
+            var entry1 = new TestDataWithListOfChildren("Max1", 
                 new List<TestSubData> {
                     sharedChild,
-                    new TestSubData("unique #1",2),
+                    new TestSubData("unique #1",2)
                 });
-            var entry2 = new TestData("Max",
+            var entry2 = new TestDataWithListOfChildren("Max2",
                 new List<TestSubData> {
                     sharedChild,
-                    new TestSubData("unique #2",3),
+                    new TestSubData("unique #2",3)
                 });
+
+            // now add the data, but try to replace existing sub-data with already existing entries
+            testDb.Add(entry1);
+
+            // just to make sure
+            await testDb.SaveChangesAsync();
+            var data1 = testDb.TestDataWithListOfChildren.Include(x => x.Children).ToArray();
+            Assert.That(data1[0].Children.Count, Is.EqualTo(2));
+
+            testDb.Add(entry2);
+
+            // Save DB
+            await testDb.SaveChangesAsync();
+
+            // ReadData and assert
+            Assert.That(testDb.TestDataWithListOfChildren.Count(), Is.EqualTo(2));  // 2x TestData
+            Assert.That(testDb.TestSubData.Count(), Is.EqualTo(3));  // 3x TestSubData
+
+            // why is the existing data modified when adding another entry
+            Assert.That(data1[0].Children.Count, Is.EqualTo(2));        // why is the shared child gone
+
+            var data2 = testDb.TestDataWithListOfChildren.Include(x => x.Children).ToArray();
+            Assert.That(data2[0].Children.Count, Is.EqualTo(2));        // why is the shared child gone
+            Assert.That(data2[1].Children.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public async Task Test_TestDataWithTwoChildren()
+        {
+            // Create DB
+            using TestDb testDb = new();
+
+            // define data
+            var sharedChild = new TestSubData("Shared", 1);
+            var entry1 = new TestDataWithTwoChildren("Max1",
+                sharedChild,
+                new TestSubData("unique #1",2)
+                );
+            var entry2 = new TestDataWithTwoChildren("Max2",
+                sharedChild,
+                new TestSubData("unique #2",3)
+                );
 
             // now add the data, but try to replace existing sub-data with already existing entries
             testDb.Add(entry1);
@@ -56,12 +104,15 @@ namespace EntitityFrameworkWithChildCollectionsIssueTests
             await testDb.SaveChangesAsync();
 
             // ReadData and assert
-            Assert.That(testDb.TestData.Count(), Is.EqualTo(2));  // 2x TestData
+            Assert.That(testDb.TestDataWithTwoChildren.Count(), Is.EqualTo(2));  // 2x TestData
             Assert.That(testDb.TestSubData.Count(), Is.EqualTo(3));  // 3x TestSubData
 
-            var data1 = testDb.TestData.Include(x => x.Children).ToArray();
-            Assert.That(data1[0].Children.Count, Is.EqualTo(2));
-            Assert.That(data1[1].Children.Count, Is.EqualTo(2));
+            // This works
+            var data2 = testDb.TestDataWithTwoChildren.Include(x => x.Child1).Include(x => x.Child2).ToArray();
+            Assert.NotNull(data2[0].Child1);
+            Assert.NotNull(data2[0].Child2);
+            Assert.NotNull(data2[1].Child1);
+            Assert.NotNull(data2[1].Child2);
         }
     }
 }
